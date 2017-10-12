1f0d0c22ba2318234d2e938501669c78128deec7
sentient64 <m.alsharekh94@gmail.com>
2017-10-13 01:29:48 +0300

    registered model Suggestion in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app.models import Event
+from app.models import Event, Suggestion
 
 # Register your models here.
 admin.site.register(Event)
+admin.site.register(Suggestion)
